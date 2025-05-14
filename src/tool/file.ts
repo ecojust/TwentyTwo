a240966743dd38d56b84cda029c17ba6edd22471
juzisang <995674251@qq.com>
2025-05-14 22:39:11 +0800

    refactor(file): change base directory from AppData to Resource
    Update the base directory used in file operations from `BaseDirectory.AppData` to `BaseDirectory.Resource` to align with the new resource management strategy. This change ensures that files are stored and accessed from the correct location.

--- src/tool/file.ts
+++ src/tool/file.ts
@@ -13,7 +13,7 @@ export default class File {
   static async _deleteFile(file: string) {
     try {
       await remove(file, {
-        baseDir: BaseDirectory.AppData,
+        baseDir: BaseDirectory.Resource,
       });
       return {
         success: true,
@@ -40,7 +40,7 @@ export default class File {
         await this._readDir(prePath);
       }
       await writeFile(file, new TextEncoder().encode(content), {
-        baseDir: BaseDirectory.AppData,
+        baseDir: BaseDirectory.Resource,
       });
       res = {
         success: true,
@@ -66,7 +66,7 @@ export default class File {
     let content = default_value || "";
     try {
       content = new TextDecoder("utf-8").decode(
-        await readFile(file, { baseDir: BaseDirectory.AppData })
+        await readFile(file, { baseDir: BaseDirectory.Resource })
       );
       res = {
         success: true,
@@ -84,13 +84,13 @@ export default class File {
     let entries: Array<DirEntry> = [];
     try {
       const res = await readDir(dir, {
-        baseDir: BaseDirectory.AppData,
+        baseDir: BaseDirectory.Resource,
       });
       entries = res;
     } catch (error) {
       await mkdir(dir, {
         recursive: true,
-        baseDir: BaseDirectory.AppData,
+        baseDir: BaseDirectory.Resource,
       });
       entries = [];
     } finally {
